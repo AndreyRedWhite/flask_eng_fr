d86fc6449423756ee30277bd8e2f2a13fd6f2fb9
AndreyRedWhite <andreyredwhite@gmail.com>
2023-07-10 18:56:41 +0300

    modifyed tests

--- final_project/machinetranslation/tests/test_translator.py
+++ final_project/machinetranslation/tests/test_translator.py
@@ -6,6 +6,7 @@ class TranslationTestCase(unittest.TestCase):
     def test_english_to_french(self):
         self.assertEqual(english_to_french("Hi"), "Bonjour")
         self.assertEqual(english_to_french("Home"), "Accueil")
+        self.assertNotEqual(english_to_french("mismatch"), "déviation")
 
     def test_french_to_english(self):
         self.assertEqual(french_to_english("Bonjour"), "Hello")
